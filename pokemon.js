a3d9a04cacea0e8275c0346c3d61602e930502bd
Neal L <rlawton.cs@gmail.com>
2024-02-14 21:36:25 -0800

    Initialized constants and added fetch call in pokemon.js.

---
+++ pokemon.js
@@ -0,0 +1,14 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemon = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+.then((response) => response.json())
+.then((data) => {
+    allPokemon = data.results;
+})
